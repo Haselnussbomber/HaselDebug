68e399a0374d8444a2b79d03d33b216a4c5a703b
Haselnussbomber <mail@haselnussbomber.de>
2024-08-10 20:38:57 +0200

    Better Texture node

--- HaselDebug/Utils/DebugUtils.cs
+++ HaselDebug/Utils/DebugUtils.cs
@@ -23,6 +23,7 @@ using HaselCommon.Utils;
 using HaselDebug.Extensions;
 using ImGuiNET;
 using InteropGenerator.Runtime.Attributes;
+using Lumina.Text;
 using Lumina.Text.ReadOnly;
 using KernelTexture = FFXIVClientStructs.FFXIV.Client.Graphics.Kernel.Texture;
 using ValueType = FFXIVClientStructs.FFXIV.Component.GUI.ValueType;
@@ -831,12 +832,6 @@ public static unsafe class DebugUtils
             title = tex->Resource->TexFileResourceHandle->ResourceHandle.FileName.ToString();
         }
 
-        using var titleColor = ImRaii.PushColor(ImGuiCol.Text, 0xFF00FFFF);
-        using var node = ImRaii.TreeNode($"{title}##Node{nodeOptions.AddressPath}", ImGuiTreeNodeFlags.SpanAvailWidth);
-        if (!node)
-            return;
-        titleColor?.Dispose();
-
         var kernelTexture = tex->GetKernelTexture();
         if (kernelTexture == null)
         {
@@ -844,7 +839,7 @@ public static unsafe class DebugUtils
             return;
         }
 
-        DrawTexture((nint)kernelTexture, new NodeOptions());
+        DrawTexture((nint)kernelTexture, new NodeOptions() { TitleOverride = new SeStringBuilder().Append(title).ToReadOnlySeString() });
     }
 
     public static void DrawTexture(nint address, NodeOptions nodeOptions)
@@ -858,8 +853,13 @@ public static unsafe class DebugUtils
         nodeOptions.EnsureAddressInPath(address);
 
         var tex = (KernelTexture*)address;
-        using var node = ImRaii.TreeNode($"{tex->Width}x{tex->Height} {tex->TextureFormat}##TextureNode{nodeOptions.AddressPath}", nodeOptions.DefaultOpen ? ImGuiTreeNodeFlags.DefaultOpen : ImGuiTreeNodeFlags.None);
+        var title = $"{tex->Width}x{tex->Height}, {(TextureFormat)tex->TextureFormat}";
+        if (nodeOptions.TitleOverride != null)
+            title = $"{nodeOptions.TitleOverride.Value.ExtractText()} ({title})";
+        using var titleColor = ImRaii.PushColor(ImGuiCol.Text, 0xFF00FFFF);
+        using var node = ImRaii.TreeNode($"{title}##TextureNode{nodeOptions.AddressPath}", nodeOptions.DefaultOpen ? ImGuiTreeNodeFlags.DefaultOpen : ImGuiTreeNodeFlags.None);
         if (!node) return;
+        titleColor?.Dispose();
 
         var size = new Vector2(tex->Width, tex->Height);
         var availSize = ImGui.GetContentRegionAvail();
@@ -867,7 +867,7 @@ public static unsafe class DebugUtils
         var scale = availSize.X / size.X;
         var scaledSize = new Vector2(size.X * scale, size.Y * scale);
 
-        ImGui.Image((nint)tex->D3D11ShaderResourceView, scaledSize);
+        ImGui.Image((nint)tex->D3D11ShaderResourceView, availSize.X < size.X ? scaledSize : size);
     }
 
     public static void DrawUtf8String(nint address, NodeOptions nodeOptions)
@@ -1882,7 +1882,8 @@ public static unsafe class DebugUtils
 
                 var columnRowType = propInfo.PropertyType.GenericTypeArguments[0];
                 var columnRowId = (uint)propInfo.PropertyType.GetProperty("RowId")?.GetValue(value)!;
-                DrawExdSheet(module, columnRowType, columnRowId, depth + 1, new NodeOptions() {
+                DrawExdSheet(module, columnRowType, columnRowId, depth + 1, new NodeOptions()
+                {
                     Language = nodeOptions.Language,
                     AddressPath = nodeOptions.AddressPath.With((columnRowType.Name.GetHashCode(), (nint)columnRowId).GetHashCode())
                 });
@@ -1971,5 +1972,49 @@ public static unsafe class DebugUtils
 
             ImGui.TextUnformatted(value.ToString());
         }
+    }
+
+    // See https://github.com/aers/FFXIVClientStructs/pull/1065
+    private enum TextureFormat : uint
+    {
+        B8G8R8A8_UNORM_4 = 0x1130,
+        A8_UNORM = 0x1131,
+        R8_UNORM = 0x1132,
+        R8_UINT = 0x1133,
+        R16_UINT = 0x1140,
+        R32_UINT = 0x1150,
+        R8G8_UNORM = 0x1240,
+        B8G8R8A8_UNORM_2 = 0x1440,
+        B8G8R8A8_UNORM_3 = 0x1441,
+        B8G8R8A8_UNORM = 0x1450,
+        B8G8R8X8_UNORM = 0x1451,
+        R16_FLOAT = 0x2140,
+        R32_FLOAT = 0x2150,
+        R16G16_FLOAT = 0x2250,
+        R32G32_FLOAT = 0x2260,
+        R11G11B10_FLOAT = 0x2350,
+        R16G16B16A16_FLOAT = 0x2460,
+        R32G32B32A32_FLOAT = 0x2470,
+        BC1_UNORM = 0x3420,
+        BC2_UNORM = 0x3430,
+        BC3_UNORM = 0x3431,
+        /// <remarks> Can also be R16_TYPELESS or R16_UNORM depending on context. </remarks>
+        D16_UNORM = 0x4140,
+        /// <remarks> Can also be R24G8_TYPELESS or R24_UNORM_X8_TYPELESS depending on context. </remarks>
+        D24_UNORM_S8_UINT = 0x4250, // depth 28 stencil 8, see MS texture formats on google if you really care :)
+        /// <remarks> Can also be R16_TYPELESS or R16_UNORM depending on context. </remarks>
+        D16_UNORM_2 = 0x5140,
+        /// <remarks> Can also be R24G8_TYPELESS or R24_UNORM_X8_TYPELESS depending on context. </remarks>
+        D24_UNORM_S8_UINT_2 = 0x5150,
+        BC4_UNORM = 0x6120,
+        BC5_UNORM = 0x6230,
+        BC6H_SF16 = 0x6330,
+        BC7_UNORM = 0x6432,
+        R16_UNORM = 0x7140,
+        R16G16_UNORM = 0x7250,
+        R10G10B10A2_UNORM_2 = 0x7350,
+        R10G10B10A2_UNORM = 0x7450,
+        /// <remarks> Can also be R24G8_TYPELESS or R24_UNORM_X8_TYPELESS depending on context. </remarks>
+        D24_UNORM_S8_UINT_3 = 0x8250,
     }
 }
