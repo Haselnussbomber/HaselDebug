d00d7499ac21411d38287260781b1c805d1f2ef4
Haselnussbomber <mail@haselnussbomber.de>
2024-08-13 22:05:09 +0200

    Fix pinned instances re-adding themselves to the saved list

--- HaselDebug/Services/PinnedInstancesService.cs
+++ HaselDebug/Services/PinnedInstancesService.cs
@@ -26,8 +26,8 @@ public class PinnedInstancesService : IReadOnlyCollection<PinnedInstanceTab>
         foreach (var name in PluginConfig.PinnedInstances)
         {
             var inst = InstancesService.Instances.FirstOrDefault(inst => inst.Type.FullName == name);
-            if (inst != null)
-                Add(inst.Address, inst.Type, false);
+            if (inst == null) continue;
+            Tabs.Add(new PinnedInstanceTab(DebugRenderer, inst.Address, inst.Type));
         }
 
         Sort();
@@ -38,9 +38,10 @@ public class PinnedInstancesService : IReadOnlyCollection<PinnedInstanceTab>
         Tabs.Sort((a, b) => a.InternalName.CompareTo(b.InternalName));
     }
 
-    public void Add(nint address, Type type, bool save = true)
+    public void Add(nint address, Type type)
     {
         Tabs.Add(new PinnedInstanceTab(DebugRenderer, address, type));
+        Sort();
 
         var nameList = new List<string>(PluginConfig.PinnedInstances)
         {
@@ -50,12 +51,7 @@ public class PinnedInstancesService : IReadOnlyCollection<PinnedInstanceTab>
         nameList.Sort();
 
         PluginConfig.PinnedInstances = [.. nameList];
-
-        if (save)
-        {
-            Sort();
-            PluginConfig.Save();
-        }
+        PluginConfig.Save();
     }
 
     public void Remove(PinnedInstanceTab tab)
