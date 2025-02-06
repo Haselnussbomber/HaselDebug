8a14ba95de29d848585eb9a942b7c5ac219c099c
Haselnussbomber <mail@haselnussbomber.de>
2025-02-06 14:46:20 +0100

    Add Territory Intended Use tab

---
+++ HaselDebug/Tabs/TerritoryIntendedUseTab.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Dalamud.Interface.Utility.Raii;
+using HaselCommon.Extensions.Strings;
+using HaselCommon.Services;
+using HaselDebug.Abstracts;
+using HaselDebug.Interfaces;
+using HaselDebug.Services;
+using HaselDebug.Utils;
+using ImGuiNET;
+using Lumina.Excel.Sheets;
+using TerritoryIntendedUseEnum = HaselCommon.Game.Enums.TerritoryIntendedUse;
+
+namespace HaselDebug.Tabs;
+
+[RegisterSingleton<IDebugTab>(Duplicate = DuplicateStrategy.Append)]
+public unsafe class TerritoryIntendedUseTab : DebugTab
+{
+    private readonly ImmutableSortedDictionary<uint, List<(TerritoryType, ContentFinderCondition[])>> Dict;
+    private readonly TextService _textService;
+    private readonly DebugRenderer _debugRenderer;
+
+    public TerritoryIntendedUseTab(ExcelService excelService, TextService textService, DebugRenderer debugRenderer)
+    {
+        _textService = textService;
+        _debugRenderer = debugRenderer;
+
+        var dict = new Dictionary<uint, List<(TerritoryType, ContentFinderCondition[])>>();
+
+        foreach (var territoryTypes in excelService.GetSheet<TerritoryType>().GroupBy(row => row.TerritoryIntendedUse.RowId))
+        {
+            var list = new List<(TerritoryType, ContentFinderCondition[])>();
+
+            foreach (var territoryType in territoryTypes)
+            {
+                list.Add((
+                    territoryType,
+                    excelService.FindRows<ContentFinderCondition>(cfcRow => cfcRow.TerritoryType.RowId == territoryType.RowId)));
+            }
+
+            dict[territoryTypes.Key] = list;
+        }
+
+        Dict = dict.ToImmutableSortedDictionary();
+    }
+
+    public override void Draw()
+    {
+        foreach (var territoryIntendedUse in Enum.GetValues<TerritoryIntendedUseEnum>())
+        {
+            if (!Dict.TryGetValue((uint)territoryIntendedUse, out var entries))
+            {
+                using (ImRaii.Disabled())
+                {
+                    using var _ = ImRaii.TreeNode($"[{(uint)territoryIntendedUse}] {territoryIntendedUse}", ImGuiTreeNodeFlags.SpanAvailWidth);
+                }
+                continue;
+            }
+
+            using var node = ImRaii.TreeNode($"[{(uint)territoryIntendedUse}] {territoryIntendedUse} ({entries.Count})", ImGuiTreeNodeFlags.SpanAvailWidth);
+            if (!node) continue;
+
+            foreach (var kv2 in entries)
+            {
+                var placeName = kv2.Item1.PlaceName.RowId != 0 && kv2.Item1.PlaceName.IsValid && !kv2.Item1.PlaceName.Value.Name.IsEmpty
+                    ? $" {_textService.GetPlaceName(kv2.Item1.PlaceName.RowId)}"
+                    : string.Empty;
+                var zoneName = kv2.Item1.PlaceNameZone.RowId != 0 && kv2.Item1.PlaceNameZone.IsValid && !kv2.Item1.PlaceNameZone.Value.Name.IsEmpty
+                    ? $" ({_textService.GetPlaceName(kv2.Item1.PlaceNameZone.RowId)})"
+                    : string.Empty;
+
+                _debugRenderer.DrawExdSheet(typeof(TerritoryType), kv2.Item1.RowId, 0, new NodeOptions()
+                {
+                    AddressPath = new AddressPath([(nint)territoryIntendedUse]),
+                    Title = $"[TerritoryType#{kv2.Item1.RowId}]{placeName}{zoneName}"
+                });
+
+                if (kv2.Item2.Length == 0)
+                    continue;
+
+                using var indent = ImRaii.PushIndent();
+
+                foreach (var cfc in kv2.Item2)
+                {
+                    _debugRenderer.DrawExdSheet(typeof(ContentFinderCondition), cfc.RowId, 0, new NodeOptions()
+                    {
+                        AddressPath = new AddressPath([(nint)territoryIntendedUse]),
+                        Title = $"[ContentFinderCondition#{cfc.RowId}] {cfc.Name.ExtractText().FirstCharToUpper().StripSoftHypen()}"
+                    });
+                }
+            }
+        }
+    }
+}
