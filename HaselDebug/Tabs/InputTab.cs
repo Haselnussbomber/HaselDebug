43228afa96a3336a4c345f566ae6be4674dfd1b3
Haselnussbomber <mail@haselnussbomber.de>
2025-05-27 11:28:24 +0200

    Add InputTab

---
+++ HaselDebug/Tabs/InputTab.cs
@@ -0,0 +1,199 @@
+using System.Numerics;
+using Dalamud.Game;
+using Dalamud.Interface.Utility.Raii;
+using FFXIVClientStructs.FFXIV.Client.System.Input;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using HaselDebug.Abstracts;
+using HaselDebug.Interfaces;
+using ImGuiNET;
+
+namespace HaselDebug.Tabs;
+
+[RegisterSingleton<IDebugTab>(Duplicate = DuplicateStrategy.Append), AutoConstruct]
+public unsafe partial class InputTab : DebugTab
+{
+    private readonly ISigScanner _sigScanner;
+
+    public override bool DrawInChild => true;
+
+    public override void Draw()
+    {
+        using var hostchild = ImRaii.Child("InputTabChild", new Vector2(-1), false, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoSavedSettings);
+        if (!hostchild) return;
+
+        using var tabbar = ImRaii.TabBar("InputTabTabBar");
+        if (!tabbar) return;
+
+        DrawInputs();
+        DrawInputIdStates();
+        DrawKeyState();
+    }
+
+    private void DrawKeyState()
+    {
+        using var tab = ImRaii.TabItem("KeyState states");
+        if (!tab) return;
+
+        using var table = ImRaii.Table("KeyStateTable", 5, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.NoSavedSettings);
+        if (!table) return;
+
+        ImGui.TableSetupColumn("SeVirtualKey", ImGuiTableColumnFlags.WidthFixed, 300);
+        ImGui.TableSetupColumn("Press", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Down", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Held", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Released", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupScrollFreeze(0, 1);
+        ImGui.TableHeadersRow();
+
+        var uiInputData = UIInputData.Instance();
+
+        foreach (var seVirtualKey in Enum.GetValues<SeVirtualKey>())
+        {
+            var isPress = uiInputData->IsKeyPressed(seVirtualKey);
+            var isDown = uiInputData->IsKeyDown(seVirtualKey);
+            var isReleased = uiInputData->IsKeyReleased(seVirtualKey);
+            var isHeld = uiInputData->IsKeyHeld(seVirtualKey);
+
+            if (!isPress && !isDown && !isReleased && !isHeld)
+                continue;
+
+            ImGui.TableNextRow();
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{seVirtualKey}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isPress}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isDown}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isHeld}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isReleased}");
+        }
+    }
+
+    private void DrawInputIdStates()
+    {
+        using var tab = ImRaii.TabItem("InputId states");
+        if (!tab) return;
+
+        using var table = ImRaii.Table("FnStateTable", 5, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.NoSavedSettings | ImGuiTableFlags.ScrollY);
+        if (!table) return;
+
+        ImGui.TableSetupColumn("InputId", ImGuiTableColumnFlags.WidthFixed, 300);
+        ImGui.TableSetupColumn("Press", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Down", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Held", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Released", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupScrollFreeze(0, 1);
+        ImGui.TableHeadersRow();
+
+        var uiInputData = UIInputData.Instance();
+
+        foreach (var inputId in Enum.GetValues<InputId>())
+        {
+            var isPress = uiInputData->IsInputIdPressed(inputId);
+            var isDown = uiInputData->IsInputIdDown(inputId);
+            var isHeld = uiInputData->IsInputIdHeld(inputId);
+            var isReleased = uiInputData->IsInputIdReleased(inputId);
+
+            if (!isPress && !isDown && !isHeld && !isReleased)
+                continue;
+
+            ImGui.TableNextRow();
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{inputId}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isPress}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isDown}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isHeld}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isReleased}");
+        }
+    }
+    
+    private void DrawInputs()
+    {
+        using var tab = ImRaii.TabItem("Inputs");
+        if (!tab) return;
+
+        /*
+        var strings = (byte**)(_sigScanner.Module.BaseAddress + 0x2716980);
+
+        if (ImGui.Button("Copy enum to clipboard"))
+        {
+            var sb = new StringBuilder();
+            var lastLineWasEmpty = false;
+
+            sb.AppendLine("public enum InputId {");
+
+            for (var i = 0; i < 675; i++)
+            {
+                var name = strings[i] != null ? new ReadOnlySeStringSpan(strings[i]).ExtractText() : string.Empty;
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    if (!lastLineWasEmpty)
+                    {
+                        sb.AppendLine();
+                        lastLineWasEmpty = true;
+                    }
+                }
+                else
+                {
+                    for (var j = 0; j < 10; j++)
+                    {
+                        if (name == j.ToString())
+                        {
+                            name = $"/// <remarks>NUM_ prefix was added for C# enum compatibility</remarks>\n    NUM_{name}";
+                            break;
+                        }
+                    }
+                    sb.AppendLine($"    {name} = {i},");
+                    lastLineWasEmpty = false;
+                }
+            }
+
+            sb.AppendLine("}");
+
+            ImGui.SetClipboardText(sb.ToString());
+        }
+        */
+
+        using var table = ImRaii.Table("InputsTable", 6, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.NoSavedSettings | ImGuiTableFlags.ScrollY);
+        if (!table) return;
+
+        ImGui.TableSetupColumn("Index", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("InputId", ImGuiTableColumnFlags.WidthFixed, 300);
+        ImGui.TableSetupColumn("Press", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Down", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Held", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Released", ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupScrollFreeze(0, 1);
+        ImGui.TableHeadersRow();
+
+        var uiInputData = UIInputData.Instance();
+
+        for (var i = 0; i < 675; i++)
+        {
+            var isPress = uiInputData->IsInputIdPressed((InputId)i);
+            var isDown = uiInputData->IsInputIdDown((InputId)i);
+            var isHeld = uiInputData->IsInputIdHeld((InputId)i);
+            var isReleased = uiInputData->IsInputIdReleased((InputId)i);
+
+            ImGui.TableNextRow();
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{i}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{(InputId)i}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isPress}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isDown}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isHeld}");
+            ImGui.TableNextColumn();
+            ImGui.TextUnformatted($"{isReleased}");
+        }
+    }
+}
