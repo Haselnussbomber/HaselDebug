477e64322f2479895d35869b9793c13f742e7a48
Haselnussbomber <mail@haselnussbomber.de>
2025-09-14 21:01:30 +0200

    Switch to hosting

--- HaselDebug/Plugin.cs
+++ HaselDebug/Plugin.cs
@@ -1,88 +1,37 @@
 using Dalamud.Plugin;
-using Dalamud.Plugin.Services;
-using HaselCommon.Commands;
-using HaselCommon.Services;
 using HaselDebug.Config;
-using HaselDebug.Windows;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace HaselDebug;
 
 public class Plugin : IDalamudPlugin
 {
     private readonly IDalamudPluginInterface _pluginInterface;
-    private readonly ServiceProvider _serviceProvider;
+    private readonly IHost _host;
 
-    public Plugin(IDalamudPluginInterface pluginInterface, IFramework framework)
+    public Plugin(IDalamudPluginInterface pluginInterface)
     {
         _pluginInterface = pluginInterface;
         _pluginInterface.InitializeCustomClientStructs();
 
-        _serviceProvider = new ServiceCollection()
-            .AddDalamud(pluginInterface)
-            .AddSingleton(PluginConfig.Load)
-            .AddHaselCommon()
-            .AddHaselDebug()
-            .BuildServiceProvider();
-
-        framework.RunOnFrameworkThread(() =>
-        {
-            if (_serviceProvider.GetRequiredService<PluginConfig>().AutoOpenPluginWindow)
-                _serviceProvider.GetRequiredService<PluginWindow>().Open();
-
-            _serviceProvider.GetRequiredService<CommandService>().Register(OnCommand, true);
-
-            _pluginInterface.UiBuilder.Draw += DrawMainMenuItem;
-            _pluginInterface.UiBuilder.OpenMainUi += TogglePluginWindow;
-            _pluginInterface.UiBuilder.OpenConfigUi += ToggleConfigWindow;
-        });
-    }
-
-    [CommandHandler("/haseldebug", "HaselDebug.CommandHandlerHelpMessage")]
-    private void OnCommand(string command, string arguments)
-    {
-        switch (arguments.Trim().ToLowerInvariant())
-        {
-            case "conf":
-            case "config":
-                ToggleConfigWindow();
-                break;
-
-            default:
-                TogglePluginWindow();
-                break;
-        }
-    }
-
-    private void DrawMainMenuItem()
-    {
-        if (_pluginInterface.IsDevMenuOpen && ImGui.BeginMainMenuBar())
-        {
-            if (ImGui.MenuItem("HaselDebug"))
+        _host = new HostBuilder()
+            .UseContentRoot(pluginInterface.AssemblyLocation.Directory!.FullName)
+            .ConfigureServices(services =>
             {
-                _serviceProvider.GetRequiredService<PluginWindow>().Toggle();
-            }
+                services.AddDalamud(pluginInterface);
+                services.AddSingleton(PluginConfig.Load);
+                services.AddHaselCommon();
+                services.AddHaselDebug();
+            })
+            .Build();
 
-            ImGui.EndMainMenuBar();
-        }
-    }
-
-    private void TogglePluginWindow()
-    {
-        _serviceProvider.GetRequiredService<PluginWindow>().Toggle();
-    }
-
-    private void ToggleConfigWindow()
-    {
-        _serviceProvider.GetRequiredService<ConfigWindow>().Toggle();
+        _host.Start();
     }
 
     void IDisposable.Dispose()
     {
-        _pluginInterface.UiBuilder.Draw -= DrawMainMenuItem;
-        _pluginInterface.UiBuilder.OpenMainUi -= TogglePluginWindow;
-        _pluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigWindow;
-
-        _serviceProvider.Dispose();
+        _host.StopAsync().GetAwaiter().GetResult();
+        _host.Dispose();
     }
 }
---
+++ HaselDebug/Services/CommandManager.cs
@@ -0,0 +1,85 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Dalamud.Plugin;
+using HaselCommon.Commands;
+using HaselCommon.Services;
+using HaselDebug.Config;
+using HaselDebug.Windows;
+using Microsoft.Extensions.Hosting;
+
+namespace HaselDebug.Services;
+
+[RegisterSingleton<IHostedService>(Duplicate = DuplicateStrategy.Append), AutoConstruct]
+public partial class CommandManager : IHostedService
+{
+    private readonly IDalamudPluginInterface _pluginInterface;
+    private readonly PluginConfig _pluginConfig;
+    private readonly WindowManager _windowManager;
+    private readonly CommandService _commandService;
+    private CommandHandler? _commandHandler;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        if (_pluginConfig.AutoOpenPluginWindow)
+            _windowManager.CreateOrOpen<PluginWindow>();
+
+        _pluginInterface.UiBuilder.Draw += DrawMainMenuItem;
+        _pluginInterface.UiBuilder.OpenMainUi += TogglePluginWindow;
+        _pluginInterface.UiBuilder.OpenConfigUi += ToggleConfigWindow;
+
+        _commandHandler = _commandService.Register(OnHaselDebugCommand, true);
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _pluginInterface.UiBuilder.Draw -= DrawMainMenuItem;
+        _pluginInterface.UiBuilder.OpenMainUi -= TogglePluginWindow;
+        _pluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigWindow;
+
+        _commandHandler?.Dispose();
+        _commandHandler = null;
+
+        return Task.CompletedTask;
+    }
+
+    private void DrawMainMenuItem()
+    {
+        if (_pluginInterface.IsDevMenuOpen && ImGui.BeginMainMenuBar())
+        {
+            if (ImGui.MenuItem("HaselDebug"))
+            {
+                TogglePluginWindow();
+            }
+
+            ImGui.EndMainMenuBar();
+        }
+    }
+
+    private void TogglePluginWindow()
+    {
+        _windowManager.CreateOrToggle<PluginWindow>();
+    }
+
+    private void ToggleConfigWindow()
+    {
+        _windowManager.CreateOrToggle<ConfigWindow>();
+    }
+
+    [CommandHandler("/haseldebug", "HaselDebug.CommandHandlerHelpMessage")]
+    private void OnHaselDebugCommand(string command, string arguments)
+    {
+        switch (arguments.Trim().ToLowerInvariant())
+        {
+            case "conf":
+            case "config":
+                ToggleConfigWindow();
+                break;
+
+            default:
+                TogglePluginWindow();
+                break;
+        }
+    }
+}
