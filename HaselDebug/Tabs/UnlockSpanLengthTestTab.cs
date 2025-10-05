f68244570eaf580729a3da1e5d0d1eec5d0cd79d
Haselnussbomber <mail@haselnussbomber.de>
2025-10-05 02:43:47 +0200

    Optimize UnlockSpanLengthTestTab

--- HaselDebug/Tabs/UnlockSpanLengthTestTab.cs
+++ HaselDebug/Tabs/UnlockSpanLengthTestTab.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Dalamud.Game;
 using Dalamud.Interface.Utility.Raii;
@@ -7,8 +8,8 @@ using HaselCommon.Services;
 using HaselDebug.Abstracts;
 using HaselDebug.Interfaces;
 using HaselDebug.Services;
+using HaselDebug.Utils;
 using Lumina.Excel.Sheets;
-using ContentRouletteSheet = Lumina.Excel.Sheets.ContentRoulette;
 
 namespace HaselDebug.Tabs;
 
@@ -18,11 +19,24 @@ public unsafe partial class UnlockSpanLengthTestTab : DebugTab
     private readonly DebugRenderer _debugRenderer;
     private readonly ExcelService _excelService;
     private readonly ISigScanner _sigScanner;
+    private readonly List<BitfieldRecord> _bitfields = [];
+    private bool _initialized;
 
     public override bool DrawInChild => false;
 
+    public record struct BitfieldRecord(string Name, int LengthHas, int NumEntries)
+    {
+        public int LengthShould = (NumEntries + 7) / 8;
+    }
+
     public override void Draw()
     {
+        if (!_initialized)
+        {
+            Initialize();
+            _initialized = true;
+        }
+
         using var table = ImRaii.Table("UnlockSpanLengthTestTable", 4, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg);
         if (!table) return;
 
@@ -32,50 +46,140 @@ public unsafe partial class UnlockSpanLengthTestTab : DebugTab
         ImGui.TableSetupColumn("Status", ImGuiTableColumnFlags.WidthFixed, 100);
         ImGui.TableHeadersRow();
 
-        static void AddRow(string name, int lengthHas, int lengthShould)
+        foreach (var entry in _bitfields)
         {
             ImGui.TableNextRow();
             ImGui.TableNextColumn();
-            ImGui.Text(name);
+            ImGuiUtilsEx.DrawCopyableText(entry.Name);
 
             ImGui.TableNextColumn();
-            ImGui.Text(lengthHas.ToString());
+            ImGuiUtilsEx.DrawCopyableText(entry.LengthHas.ToString());
 
             ImGui.TableNextColumn();
-            ImGui.Text(lengthShould.ToString());
+            ImGuiUtilsEx.DrawCopyableText(entry.LengthShould.ToString());
 
             ImGui.TableNextColumn();
-            var match = lengthHas == lengthShould;
+            var match = entry.LengthHas == entry.LengthShould;
             using var colorOk = Color.Green.Push(ImGuiCol.Text, match);
             using var colorMismatch = Color.Red.Push(ImGuiCol.Text, !match);
             ImGui.Text(match ? "OK" : "MISMATCH");
         }
+    }
 
-        var playerState = PlayerState.Instance();
-        var uiState = UIState.Instance();
+    private void Initialize()
+    {
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedMountsBitmask",
+            PlayerState.Instance()->UnlockedMountsBitmask.Length,
+            _excelService.GetSheet<Mount>().Where(row => row.ModelChara.RowId != 0).Max(row => row.Order)));
 
-        AddRow("PlayerState.UnlockedMountsBitmask", playerState->UnlockedMountsBitmask.Length, (_excelService.GetSheet<Mount>().Where(row => row.ModelChara.RowId != 0).Max(row => row.Order) + 7) >> 3);
-        AddRow("PlayerState.UnlockedOrnamentsBitmask", playerState->UnlockedOrnamentsBitmask.Length, (_excelService.GetRowCount<Ornament>() + 7) >> 3);
-        AddRow("PlayerState.UnlockedGlassesStylesBitmask", playerState->UnlockedGlassesStylesBitmask.Length, (_excelService.GetRowCount<GlassesStyle>() + 7) >> 3);
-        AddRow("PlayerState.UnlockedFishingSpotBitmask", playerState->UnlockedFishingSpotBitmask.Length, (_excelService.GetSheet<FishingSpot>().Max(row => row.Order) + 7) >> 3);
-        AddRow("PlayerState.CaughtFishBitmask", playerState->CaughtFishBitmask.Length, (_excelService.GetSheet<FishParameter>().Count(row => row.IsInLog) + 7) >> 3);
-        AddRow("PlayerState.UnlockedSpearfishingNotebookBitmask", playerState->UnlockedSpearfishingNotebookBitmask.Length, (_excelService.GetRowCount<SpearfishingNotebook>() + 7) >> 3);
-        AddRow("PlayerState.CaughtSpearfishBitmask", playerState->CaughtSpearfishBitmask.Length, (int)(_excelService.GetSheet<SpearfishingItem>().Where(row => row.RowId < 30000).Max(row => row.RowId) - 20000 + 7) >> 3);
-        AddRow("PlayerState.ContentRouletteCompletion", playerState->ContentRouletteCompletion.Length, _excelService.GetSheet<ContentRouletteSheet>().Max(row => row.Unknown17)); // Not a bit array. TODO: CompletionArrayIndex
-        AddRow("PlayerState.UnlockedSecretRecipeBooksBitmask", playerState->UnlockedSecretRecipeBooksBitmask.Length, (_excelService.GetRowCount<SecretRecipeBook>() + 7) >> 3);
-        AddRow("PlayerState.CompletedAdventureBitmask", playerState->CompletedAdventureBitmask.Length, (_excelService.GetRowCount<Adventure>() + 7) >> 3);
-        AddRow("PlayerState.UnlockedAetherCurrentsBitmask", playerState->UnlockedAetherCurrentsBitmask.Length, (_excelService.GetRowCount<AetherCurrent>() + 7) >> 3);
-        AddRow("PlayerState.UnlockedAetherCurrentCompFlgSetBitmask", playerState->UnlockedAetherCurrentCompFlgSetBitmask.Length, _excelService.GetRowCount<AetherCurrentCompFlgSet>() >> 3);
-        AddRow("PlayerState.UnlockedMinerFolkloreTomeBitmask", playerState->UnlockedMinerFolkloreTomeBitmask.Length, (int)(_excelService.GetSheet<GatheringSubCategory>().Where(row => row.ClassJob.RowId == 16 && row.Quest.RowId < 74).Max(row => row.Quest.RowId) + 7) >> 3);
-        AddRow("PlayerState.UnlockedBotanistFolkloreTomeBitmask", playerState->UnlockedBotanistFolkloreTomeBitmask.Length, (int)(_excelService.GetSheet<GatheringSubCategory>().Where(row => row.ClassJob.RowId == 17 && row.Quest.RowId < 74).Max(row => row.Quest.RowId) + 7) >> 3);
-        AddRow("PlayerState.UnlockedFishingFolkloreTomeBitmask", playerState->UnlockedFishingFolkloreTomeBitmask.Length, (int)(_excelService.GetSheet<GatheringSubCategory>().Where(row => row.ClassJob.RowId == 18 && row.Quest.RowId < 74).Max(row => row.Quest.RowId) + 7) >> 3);
-        AddRow("PlayerState.UnlockedOrchestrionRollBitmask", playerState->UnlockedOrchestrionRollBitmask.Length, (_excelService.GetRowCount<Orchestrion>() + 7) >> 3);
-        // AddRow("PlayerState.UnlockedFramersKitsBitmask", playerState->UnlockedFramersKitsBitmask.Length, unknown);
-        AddRow("UIState.UnlockedAetherytesBitmask", uiState->UnlockedAetherytesBitmask.Length, (_excelService.GetRowCount<Aetheryte>() + 7) >> 3);
-        AddRow("UIState.UnlockedHowtoBitmask", uiState->UnlockedHowtoBitmask.Length, (_excelService.GetRowCount<HowTo>() + 7) >> 3);
-        AddRow("UIState.UnlockedCompanionsBitmask", uiState->UnlockedCompanionsBitmask.Length, (_excelService.GetRowCount<Companion>() + 7) >> 3);
-        AddRow("UIState.ChocoboTaxiStandsBitmask", uiState->UnlockedChocoboTaxiStandsBitmask.Length, (_excelService.GetRowCount<ChocoboTaxiStand>() + 7) >> 3);
-        AddRow("UIState.CutsceneSeenBitmask", uiState->CutsceneSeenBitmask.Length, (_excelService.GetSheet<CutsceneWorkIndex>().Max(row => row.WorkIndex) + 7) >> 3);
-        AddRow("UIState.UnlockedTripleTriadCardsBitmask", uiState->UnlockedTripleTriadCardsBitmask.Length, (_excelService.GetRowCount<TripleTriadCard>() + 7) >> 3);
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedMountsBitmask NEW",
+            PlayerState.Instance()->UnlockedMountsBitmask.Length,
+            _excelService.GetSheet<Mount>().Where(row => row.ModelChara.RowId != 0).Max(row => row.Order)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedOrnamentsBitmask",
+            PlayerState.Instance()->UnlockedOrnamentsBitmask.Length,
+            _excelService.GetRowCount<Ornament>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedGlassesStylesBitmask",
+            PlayerState.Instance()->UnlockedGlassesStylesBitmask.Length,
+            _excelService.GetRowCount<GlassesStyle>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedFishingSpotBitmask",
+            PlayerState.Instance()->UnlockedFishingSpotBitmask.Length,
+            _excelService.GetSheet<FishingSpot>().Max(row => row.Order)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.CaughtFishBitmask",
+            PlayerState.Instance()->CaughtFishBitmask.Length,
+            _excelService.GetSheet<FishParameter>().Count(row => row.IsInLog)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedSpearfishingNotebookBitmask",
+            PlayerState.Instance()->UnlockedSpearfishingNotebookBitmask.Length,
+            _excelService.GetRowCount<SpearfishingNotebook>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.CaughtSpearfishBitmask",
+            PlayerState.Instance()->CaughtSpearfishBitmask.Length,
+            (int)(_excelService.GetSheet<SpearfishingItem>().Where(row => row.RowId < 30000).Max(row => row.RowId) - 20000)));
+
+        // _bitfields.Add(new BitfieldRecord(
+        //     "PlayerState.ContentRouletteCompletion",
+        //     PlayerState.Instance()->ContentRouletteCompletion.Length,
+        //     _excelService.GetSheet<ContentRouletteSheet>().Max(row => row.Unknown17))); // Not a bit array. TODO: CompletionArrayIndex
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedSecretRecipeBooksBitmask",
+            PlayerState.Instance()->UnlockedSecretRecipeBooksBitmask.Length,
+            _excelService.GetRowCount<SecretRecipeBook>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.CompletedAdventureBitmask",
+            PlayerState.Instance()->CompletedAdventureBitmask.Length,
+            _excelService.GetRowCount<Adventure>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedAetherCurrentsBitmask",
+            PlayerState.Instance()->UnlockedAetherCurrentsBitmask.Length,
+            _excelService.GetRowCount<AetherCurrent>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedAetherCurrentCompFlgSetBitmask",
+            PlayerState.Instance()->UnlockedAetherCurrentCompFlgSetBitmask.Length,
+            _excelService.GetSheet<AetherCurrentCompFlgSet>().Count(row => row.Territory.IsValid)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedMinerFolkloreTomeBitmask",
+            PlayerState.Instance()->UnlockedMinerFolkloreTomeBitmask.Length,
+            (int)_excelService.GetSheet<GatheringSubCategory>().Where(row => row.ClassJob.RowId == 16 && row.Quest.RowId < 74).Max(row => row.Quest.RowId)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedBotanistFolkloreTomeBitmask",
+            PlayerState.Instance()->UnlockedBotanistFolkloreTomeBitmask.Length,
+            (int)_excelService.GetSheet<GatheringSubCategory>().Where(row => row.ClassJob.RowId == 17 && row.Quest.RowId < 74).Max(row => row.Quest.RowId)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedFishingFolkloreTomeBitmask",
+            PlayerState.Instance()->UnlockedFishingFolkloreTomeBitmask.Length,
+            (int)_excelService.GetSheet<GatheringSubCategory>().Where(row => row.ClassJob.RowId == 18 && row.Quest.RowId < 74).Max(row => row.Quest.RowId)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "PlayerState.UnlockedOrchestrionRollBitmask",
+            PlayerState.Instance()->UnlockedOrchestrionRollBitmask.Length,
+            _excelService.GetRowCount<Orchestrion>()));
+
+        // _bitfields.Add(new BitfieldRecord(
+        //    "PlayerState.UnlockedFramersKitsBitmask",
+        //    PlayerState->UnlockedFramersKitsBitmask.Length,
+        //    unknown));
+
+        _bitfields.Add(new BitfieldRecord(
+            "UIState.UnlockedAetherytesBitmask", UIState.Instance()->UnlockedAetherytesBitmask.Length,
+            _excelService.GetRowCount<Aetheryte>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "UIState.UnlockedHowtoBitmask", UIState.Instance()->UnlockedHowtoBitmask.Length,
+            _excelService.GetRowCount<HowTo>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "UIState.UnlockedCompanionsBitmask", UIState.Instance()->UnlockedCompanionsBitmask.Length,
+            _excelService.GetRowCount<Companion>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "UIState.ChocoboTaxiStandsBitmask", UIState.Instance()->UnlockedChocoboTaxiStandsBitmask.Length,
+            _excelService.GetRowCount<ChocoboTaxiStand>()));
+
+        _bitfields.Add(new BitfieldRecord(
+            "UIState.CutsceneSeenBitmask", UIState.Instance()->CutsceneSeenBitmask.Length,
+            _excelService.GetSheet<CutsceneWorkIndex>().Max(row => row.WorkIndex)));
+
+        _bitfields.Add(new BitfieldRecord(
+            "UIState.UnlockedTripleTriadCardsBitmask", UIState.Instance()->UnlockedTripleTriadCardsBitmask.Length,
+            _excelService.GetRowCount<TripleTriadCard>()));
     }
 }
