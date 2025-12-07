54a89a7fdd314037a4efb4b704350e8580625b2a
Haselnussbomber <mail@haselnussbomber.de>
2025-12-07 02:09:47 +0100

    Find size from vf0

--- HaselDebug/Tabs/PointerInspectorTab.cs
+++ HaselDebug/Tabs/PointerInspectorTab.cs
@@ -6,13 +6,15 @@ using HaselDebug.Abstracts;
 using HaselDebug.Interfaces;
 using HaselDebug.Services;
 using HaselDebug.Utils;
+using Iced.Intel;
 
 namespace HaselDebug.Tabs;
 
 [RegisterSingleton<IDebugTab>(Duplicate = DuplicateStrategy.Append), AutoConstruct]
-public partial class PointerInspectorTab : DebugTab
+public unsafe partial class PointerInspectorTab : DebugTab
 {
     private readonly DebugRenderer _debugRenderer;
+    private readonly ISigScanner _sigScanner;
     private readonly ILogger<PointerInspectorTab> _logger;
 
     private readonly Dictionary<nint, Type> _virtualTableMappings = [];
@@ -24,12 +26,13 @@ public partial class PointerInspectorTab : DebugTab
 
     private uint _memorySize;
     private nint _memoryAddress;
+    private nint _freeMemoryAddress;
 
     private record OffsetInfo(nint ActualAddress, int Offset);
 
     public override void Draw()
     {
-        _loadTask ??= Task.Run(PopulateVirtualTableMappings);
+        _loadTask ??= Task.Run(Load);
 
         if (!_loadTask.IsCompleted)
         {
@@ -74,6 +77,7 @@ public partial class PointerInspectorTab : DebugTab
         if (ImGui.InputText("##MemoryAddress", ref _addressInput, 20, ImGuiInputTextFlags.AutoSelectAll))
         {
             _memoryAddress = ParseNumericString<nint>(_addressInput);
+            FindSize();
         }
 
         ImGui.TableNextColumn();
@@ -100,7 +104,7 @@ public partial class PointerInspectorTab : DebugTab
         ImGui.Text($"Struct Address: 0x{_memoryAddress:X}\nStruct Size: 0x{_memorySize:X}");
     }
 
-    private unsafe void ParsePointer(IntPtr address, uint size)
+    private void ParsePointer(IntPtr address, uint size)
     {
         _offsetMappings.Clear();
 
@@ -160,8 +164,10 @@ public partial class PointerInspectorTab : DebugTab
         }
     }
 
-    private void PopulateVirtualTableMappings()
+    private void Load()
     {
+        _freeMemoryAddress = _sigScanner.ScanText("E8 ?? ?? ?? ?? 4D 89 AE") - _sigScanner.Module.BaseAddress;
+
         foreach (var type in typeof(AtkUnitBase).Assembly.GetTypes())
         {
             if (!type.GetCustomAttributes<VirtualTableAttribute>(false).Any())
@@ -182,5 +188,84 @@ public partial class PointerInspectorTab : DebugTab
             if (_virtualTableMappings.TryAdd(address.Value, type))
                 _logger.LogDebug("Mapped: {address:X} to {typeName}", address.Value, type.Name);
         }
+    }
+
+    private void FindSize()
+    {
+        if (!MemoryUtils.IsPointerValid(_memoryAddress))
+        {
+            _logger.LogWarning("FindSize failed at _memoryAddress ({_memoryAddress:X})", _memoryAddress);
+            return;
+        }
+
+        var vtblPtr = *(nint*)_memoryAddress;
+        if (!MemoryUtils.IsPointerValid(vtblPtr))
+        {
+            _logger.LogWarning("FindSize failed at vtblPtr ({vtblPtr:X})", vtblPtr);
+            return;
+        }
+
+        var vf0Ptr = *(nint*)vtblPtr;
+        if (!MemoryUtils.IsPointerValid(vf0Ptr))
+        {
+            _logger.LogWarning("FindSize failed at vf0Ptr ({vf0Ptr:X})", vf0Ptr);
+            return;
+        }
+
+        _logger.LogDebug("Decoding vf0 @ {addr:X}", vf0Ptr);
+
+        var codeReader = new NativeCodeReader(vf0Ptr);
+        var decoder = Decoder.Create(64, codeReader);
+        decoder.IP = (ulong)(vf0Ptr - _sigScanner.Module.BaseAddress);
+
+        var vtblAddress = nint.Zero;
+        var rax = nint.Zero;
+        
+        var list = new List<Instruction>();
+
+        while (codeReader.CanReadByte)
+        {
+            decoder.Decode(out var instr);
+
+            if (instr.IsInvalid || instr.FlowControl == FlowControl.Return)
+                break;
+
+            list.Add(instr);
+        }
+
+        list.Reverse();
+
+        var isFreeMemoryCall = false;
+        var remainingInstructions = 0;
+        foreach (var instr in list.Take(20))
+        {
+            _logger.LogTrace("Instruction @ 0x{addr:X}: {instr}", instr.IP, instr.ToString());
+
+            if (instr.Code == Code.Call_rel32_64 && instr.NearBranch32 == _freeMemoryAddress)
+            {
+                _logger.LogDebug("Found FreeMemory call @ 0x{addr:X}", instr.IP);
+                isFreeMemoryCall = true;
+                continue;
+            }
+
+            if (!isFreeMemoryCall)
+                continue;
+
+            if (--remainingInstructions == 0)
+                break;
+
+            if (instr.Code == Code.Mov_r32_imm32
+                && instr.Op0Kind == OpKind.Register
+                && instr.Op0Register == Register.EDX
+                && instr.Op1Kind == OpKind.Immediate32)
+            {
+                _memorySize = instr.Immediate32;
+                _addressSize = "0x" + _memorySize.ToString("X");
+                _logger.LogDebug("Found struct size {size} @ 0x{addr:X}", _addressSize, instr.IP);
+                return;
+            }
+        }
+
+        _logger.LogDebug("Struct size not found");
     }
 }
