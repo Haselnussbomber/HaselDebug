5094b62524acfafea3e7e6e60899aaf45beb05e1
Haselnussbomber <mail@haselnussbomber.de>
2025-12-26 22:54:17 +0100

    Add copy button to CurrencyManagerTab

--- HaselDebug/Tabs/CurrencyManagerTab.cs
+++ HaselDebug/Tabs/CurrencyManagerTab.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.STD;
 using HaselDebug.Abstracts;
 using HaselDebug.Interfaces;
 using HaselDebug.Services;
 using HaselDebug.Utils;
+using static FFXIVClientStructs.FFXIV.Client.Game.CurrencyManager;
 
 namespace HaselDebug.Tabs;
 
@@ -25,6 +28,8 @@ public unsafe partial class CurrencyManagerTab : DebugTab
         {
             if (node)
             {
+                DrawCopyButton("Copy SpecialItemBucket"u8, ref currencyManager->SpecialItemBucket);
+
                 using var table = ImRaii.Table(nameof(CurrencyManager.SpecialItemBucket) + "Table", 5, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg);
                 if (table)
                 {
@@ -58,6 +63,8 @@ public unsafe partial class CurrencyManagerTab : DebugTab
         {
             if (node)
             {
+                DrawCopyButton("Copy ItemBucket"u8, ref currencyManager->ItemBucket);
+
                 using var table = ImRaii.Table(nameof(CurrencyManager.ItemBucket) + "Table", 5, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg);
                 if (table)
                 {
@@ -76,7 +83,7 @@ public unsafe partial class CurrencyManagerTab : DebugTab
                         ImGui.TableNextColumn();
                         ImGui.Text($"{item.Count} / {item.MaxCount}");
                         ImGui.TableNextColumn();
-                        if (currencyManager->IsItemLimited(itemId))
+                        if (!item.IsUnlimited)
                             ImGui.Text(currencyManager->GetItemCountRemaining(itemId).ToString());
                         ImGui.TableNextColumn();
                         ImGui.Text(item.IsUnlimited.ToString());
@@ -91,6 +98,8 @@ public unsafe partial class CurrencyManagerTab : DebugTab
         {
             if (node)
             {
+                DrawCopyButton("Copy ContentItemBucket"u8, ref currencyManager->ContentItemBucket);
+
                 using var table = ImRaii.Table(nameof(CurrencyManager.ContentItemBucket) + "Table", 5, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg);
                 if (table)
                 {
@@ -109,7 +118,7 @@ public unsafe partial class CurrencyManagerTab : DebugTab
                         ImGui.TableNextColumn();
                         ImGui.Text($"{item.Count} / {item.MaxCount}");
                         ImGui.TableNextColumn();
-                        if (currencyManager->IsItemLimited(itemId))
+                        if (!item.IsUnlimited)
                             ImGui.Text(currencyManager->GetItemCountRemaining(itemId).ToString());
                         ImGui.TableNextColumn();
                         ImGui.Text(item.IsUnlimited.ToString());
@@ -119,5 +128,119 @@ public unsafe partial class CurrencyManagerTab : DebugTab
                 }
             }
         }
+    }
+
+    private void DrawCopyButton(ReadOnlySpan<byte> label, ref StdMap<uint, SpecialCurrencyItem> map)
+    {
+        if (!ImGui.Button(label))
+            return;
+
+        var longestName = 0;
+        foreach (var (itemId, _) in map)
+        {
+            var nameLength = _itemService.GetItemName(itemId, false, ClientLanguage.English).ToString().Length;
+            if (longestName < nameLength)
+                longestName = nameLength;
+        }
+
+        var sb = new StringBuilder(@$"
+    /// <remarks>
+    /// This bucket is known to contain the following items:<br/>
+    /// <code>
+    /// |-----------|--------|-{new string('-', longestName)}-|<br/>
+    /// | SpecialId | ItemId | {"Item Name".PadRight(longestName)} |<br/>
+    /// |-----------|--------|-{new string('-', longestName)}-|<br/>
+");
+
+        foreach (var (itemId, item) in map.OrderBy(kv => kv.Value.SpecialId))
+        {
+            var name = _itemService.GetItemName(itemId, false, ClientLanguage.English).ToString();
+            sb.AppendLine($"    /// | {item.SpecialId,-9} | {itemId,-6} | {name.PadRight(longestName)} |<br/>");
+        }
+
+        sb.Append(@$"    /// |-----------|--------|-{new string('-', longestName)}-|
+    /// </code>
+    /// </remarks>");
+
+        ImGui.SetClipboardText(sb.ToString());
+    }
+
+    private void DrawCopyButton(ReadOnlySpan<byte> label, ref StdMap<uint, CurrencyItem> map)
+    {
+        if (!ImGui.Button(label))
+            return;
+
+        var longestName = 0;
+        foreach (var (itemId, _) in map)
+        {
+            var nameLength = _itemService.GetItemName(itemId, false, ClientLanguage.English).ToString().Length;
+            if (longestName < nameLength)
+                longestName = nameLength;
+        }
+
+        var sb = new StringBuilder(@$"
+    /// <remarks>
+    /// This bucket is known to contain the following items:<br/>
+    /// <code>
+    /// |--------|-{new string('-', longestName)}-|<br/>
+    /// | ItemId | {"Item Name".PadRight(longestName)} |<br/>
+    /// |--------|-{new string('-', longestName)}-|<br/>
+");
+
+        foreach (var (itemId, _) in map.OrderBy(kv => kv.Key))
+        {
+            var name = _itemService.GetItemName(itemId, false, ClientLanguage.English).ToString();
+            sb.AppendLine($"    /// | {itemId,-6} | {name.PadRight(longestName)} |<br/>");
+        }
+
+        sb.Append(@$"    /// |--------|-{new string('-', longestName)}-|
+    /// </code>
+    /// </remarks>");
+
+        ImGui.SetClipboardText(sb.ToString());
+    }
+
+    private void DrawCopyButton(ReadOnlySpan<byte> label, ref StdMap<uint, ContentCurrencyItem> map)
+    {
+        if (!ImGui.Button(label))
+            return;
+
+        var longestName = 0;
+        foreach (var (itemId, _) in map)
+        {
+            var name = itemId switch
+            {
+                33138 => "(YoRHa Questline Progress)",
+                _ => _itemService.GetItemName(itemId, false, ClientLanguage.English).ToString()
+            };
+            var nameLength = name.Length;
+            if (longestName < nameLength)
+                longestName = nameLength;
+        }
+
+        var sb = new StringBuilder(@$"
+    /// <remarks>
+    /// This bucket is known to contain the following items:<br/>
+    /// <code>
+    /// |--------|-{new string('-', longestName)}-|<br/>
+    /// | ItemId | {"Item Name".PadRight(longestName)} |<br/>
+    /// |--------|-{new string('-', longestName)}-|<br/>
+");
+
+        foreach (var (itemId, _) in map.OrderBy(kv => kv.Key))
+        {
+            var name = itemId switch
+            {
+                33138 => "(YoRHa Questline Progress)",
+                _ => _itemService.GetItemName(itemId, false, ClientLanguage.English).ToString()
+            };
+            sb.AppendLine($"    /// | {itemId,-6} | {name.PadRight(longestName)} |<br/>");
+        }
+
+        sb.Append(@$"    /// |--------|-{new string('-', longestName)}-|
+    /// </code>
+    /// </remarks>");
+
+        ImGui.SetClipboardText(sb.ToString());
     }
 }
