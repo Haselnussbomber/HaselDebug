b7cb79b64252d88ed362ced0b25aafe92729c36e
Haselnussbomber <mail@haselnussbomber.de>
2025-12-27 14:22:07 +0100

    Update PartListSelector

--- HaselDebug/Utils/ImGuiUtilsEx.cs
+++ HaselDebug/Utils/ImGuiUtilsEx.cs
@@ -1,10 +1,13 @@
 using Dalamud.Interface.Textures;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using HaselDebug.Services;
 
 namespace HaselDebug.Utils;
 
 public static unsafe class ImGuiUtilsEx
 {
+    private static int TexDisplayStyle = 0;
+
     public static bool EnumCombo<T>(string label, ref T refValue, bool flagCombo = false) where T : Enum
     {
         if (flagCombo)
@@ -83,83 +86,251 @@ public static unsafe class ImGuiUtilsEx
         ref var currentPart = ref partsList->Parts[partId];
         var changed = false;
 
-        /* not sure yet
-        using (var combo = ImRaii.Combo("##PartId", $"[{partId}] {GetPartLabel(currentPart)}"))
-        {
-            if (combo)
-            {
-                for (var i = 0u; i < partsList->PartCount; i++)
-                {
-                    ref var part = ref partsList->Parts[i];
-                    var isSelected = i == partId;
-
-                    if (ImGui.Selectable($"[{i}] {GetPartLabel(part)}", isSelected))
-                    {
-                        partId = i;
-                        changed = true;
-                    }
-
-                    if (isSelected)
-                    {
-                        ImGui.SetItemDefaultFocus();
-                    }
-                }
-            }
-        }
-        */
-
         PrintFieldValuePairs(
             ("PartId", partId.ToString()),
             ("Position", $"({currentPart.U}, {currentPart.V})"),
             ("Size", $"{currentPart.Width}x{currentPart.Height}")
         );
 
-        var textureInfo = partsList->Parts[partId].UldAsset;
-        var texType = textureInfo->AtkTexture.TextureType;
+        var asset = currentPart.UldAsset;
+        var texType = asset->AtkTexture.TextureType;
 
-        if (texType == TextureType.Resource)
+        if (!asset->AtkTexture.IsTextureReady())
         {
-            ImGuiUtils.DrawCopyableText(textureInfo->AtkTexture.Resource->TexFileResourceHandle->ResourceHandle.FileName.ToString());
+            ImGui.Text("Texture not ready"u8);
+            return changed;
+        }
 
-            /* explodes
-            if (textureInfo->AtkTexture.Resource->IconId != 0)
+        var path = string.Empty;
+        var version = 1;
+        if (asset->AtkTexture.TextureType == TextureType.Resource)
+        {
+            path = asset->AtkTexture.Resource->TexFileResourceHandle->ResourceHandle.FileName.ToString();
+            version = asset->AtkTexture.Resource->Version;
+        }
+
+        var kernelTexture = asset->AtkTexture.GetKernelTexture();
+        if (kernelTexture == null)
+        {
+            ImGui.Text("No KernelTexture"u8);
+            return changed;
+        }
+
+        if (!ServiceLocator.TryGetService<DebugRenderer>(out var debugRenderer))
+            return changed;
+
+        var tex = asset->AtkTexture.GetKernelTexture();
+        var nodeOptions = new NodeOptions
+        {
+            AddressPath = new AddressPath((nint)asset),
+            DefaultOpen = true,
+            DrawContextMenu = (nodeOptions, builder) =>
             {
-                var iconId = (int)textureInfo->AtkTexture.Resource->IconId;
-                if (ImGui.InputInt("IconId", ref iconId))
+                builder.Add(new ImGuiContextMenuEntry()
                 {
-                    textureInfo->AtkTexture.LoadIconTexture((uint)iconId);
+                    Visible = !string.IsNullOrEmpty(path),
+                    Label = "Copy Path",
+                    ClickCallback = () => ImGui.SetClipboardText(path)
+                });
+
+                builder.Add(new ImGuiContextMenuEntry()
+                {
+                    Label = "Copy Size",
+                    ClickCallback = () => ImGui.SetClipboardText(ImGui.IsKeyDown(ImGuiKey.LeftShift)
+                        ? $"new Vector2({tex->ActualWidth}, {tex->ActualHeight})"
+                        : $"{tex->ActualWidth}x{tex->ActualHeight}")
+                });
+
+                builder.Add(new ImGuiContextMenuEntry()
+                {
+                    Label = "Copy Format",
+                    ClickCallback = () => ImGui.SetClipboardText($"{tex->TextureFormat}")
+                });
+            }
+        };
+
+        var texInfo = $"{tex->ActualWidth}x{tex->ActualHeight}, {tex->TextureFormat}";
+        nodeOptions.Title = string.IsNullOrEmpty(path)
+            ? texInfo
+            : $"{path} ({texInfo})";
+
+        using var node = debugRenderer.DrawTreeNode(nodeOptions);
+        if (!node)
+            return changed;
+
+        if (ImGui.RadioButton("Parts List##TextureDisplayStyle0"u8, TexDisplayStyle == 0))
+        {
+            TexDisplayStyle = 0;
+        }
+
+        ImGui.SameLine();
+
+        if (ImGui.RadioButton("Full Image##TextureDisplayStyle1"u8, TexDisplayStyle == 1))
+        {
+            TexDisplayStyle = 1;
+        }
+
+        var size = new Vector2(tex->ActualWidth, tex->ActualHeight);
+
+        if (TexDisplayStyle == 1)
+        {
+            var pos = ImGui.GetCursorPos();
+            var screenPos = ImGui.GetCursorScreenPos();
+
+            ImGui.Image(new ImTextureID(tex->D3D11ShaderResourceView), size);
+
+            var posAfter = ImGui.GetCursorPos();
+            var drawList = ImGui.GetWindowDrawList();
+
+            for (var i = 0u; i < partsList->PartCount; i++)
+            {
+                var part = partsList->Parts[i];
+                var partPos = new Vector2(part.U, part.V) * version;
+                var partSize = new Vector2(part.Width, part.Height) * version;
+                var partSelected = i == partId;
+
+                drawList.AddRect(
+                    screenPos + partPos,
+                    screenPos + partPos + partSize,
+                    partSelected
+                        ? Color.Gold.ToUInt()
+                        : Color.Grey4.ToUInt());
+
+                ImGui.SetCursorPos(pos + partPos);
+                ImGui.Dummy(partSize);
+                var popupKey = $"##Asset{(nint)asset}_Part{i}";
+
+                if (ImGui.IsItemHovered() || ImGui.IsPopupOpen(popupKey))
+                {
+                    var text = $"#{i}";
+                    var textSize = ImGui.CalcTextSize(text);
+                    var textPos = screenPos + partPos + partSize / 2f - textSize / 2f;
+
+                    drawList.AddRectFilled(
+                        screenPos + partPos + Vector2.One,
+                        screenPos + partPos + partSize - Vector2.One,
+                        (Color.Black with { A = 0.5f }).ToUInt());
+
+                    drawList.AddText(
+                        textPos - Vector2.One,
+                        Color.Black.ToUInt(),
+                        text);
+
+                    drawList.AddText(
+                        textPos + Vector2.One,
+                        Color.Black.ToUInt(),
+                        text);
+
+                    drawList.AddText(
+                        textPos,
+                        Color.Gold.ToUInt(),
+                        text);
+                }
+
+                if (ImGui.IsItemClicked(ImGuiMouseButton.Left))
+                {
+                    partId = i;
+                    changed |= true;
+                }
+
+                if (ServiceLocator.TryGetService<ImGuiContextMenuService>(out var contextMenuService))
+                {
+                    contextMenuService.Draw(popupKey, builder =>
+                    {
+                        builder.Add(new ImGuiContextMenuEntry()
+                        {
+                            Label = "Copy PartId",
+                            ClickCallback = () => ImGui.SetClipboardText(i.ToString())
+                        });
+                        builder.Add(new ImGuiContextMenuEntry()
+                        {
+                            Label = "Copy Position",
+                            ClickCallback = () => ImGui.SetClipboardText(ImGui.IsKeyDown(ImGuiKey.LeftShift)
+                                ? $"new Vector2({part.U}, {part.V})"
+                                : $"({part.U}, {part.V})")
+                        });
+                        builder.Add(new ImGuiContextMenuEntry()
+                        {
+                            Label = "Copy Size",
+                            ClickCallback = () => ImGui.SetClipboardText(ImGui.IsKeyDown(ImGuiKey.LeftShift)
+                                ? $"new Vector2({part.Width}, {part.Height})"
+                                : $"{part.Width}x{part.Height}")
+                        });
+                    });
                 }
             }
-            */
 
-            ref var kernelTexture = ref textureInfo->AtkTexture.Resource->KernelTextureObject;
-
-            using var treeNode = ImRaii.TreeNode(
-                $"Texture##{(ulong)kernelTexture->D3D11ShaderResourceView:X}",
-                ImGuiTreeNodeFlags.SpanAvailWidth | ImGuiTreeNodeFlags.DefaultOpen);
-
-            if (treeNode)
-            {
-                ImGui.Image(
-                    new ImTextureID(kernelTexture->D3D11ShaderResourceView),
-                    new Vector2(
-                        kernelTexture->ActualWidth,
-                        kernelTexture->ActualHeight));
-            }
+            ImGui.SetCursorPos(posAfter);
         }
-        else if (texType == TextureType.KernelTexture)
+        else
         {
-            using var treeNode = ImRaii.TreeNode(
-                $"Texture##{(ulong)textureInfo->AtkTexture.KernelTexture->D3D11ShaderResourceView:X}",
-                ImGuiTreeNodeFlags.SpanAvailWidth | ImGuiTreeNodeFlags.DefaultOpen);
+            using var tbl = ImRaii.Table($"partsTable##{(nint)asset:X}", 3, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.Reorderable);
+            if (!tbl)
+                return changed;
 
-            if (treeNode)
+            var maxPartWidth = 60;
+
+            for (var i = 0u; i < partsList->PartCount; i++)
             {
+                ref var part = ref partsList->Parts[i];
+                if (maxPartWidth < part.Width * version)
+                    maxPartWidth = part.Width * version;
+            }
+
+            ImGui.TableSetupColumn("PartId"u8, ImGuiTableColumnFlags.WidthFixed, 40);
+            ImGui.TableSetupColumn("Texture"u8, ImGuiTableColumnFlags.WidthFixed, maxPartWidth);
+            ImGui.TableSetupColumn("Info"u8, ImGuiTableColumnFlags.WidthStretch);
+            ImGui.TableHeadersRow();
+
+            for (var i = 0u; i < partsList->PartCount; i++)
+            {
+                ref var part = ref partsList->Parts[i];
+                var selected = i == partId;
+
+                ImGui.TableNextRow();
+
+                ImGui.TableNextColumn(); // PartId
+                if (selected) ImGui.TableSetBgColor(ImGuiTableBgTarget.CellBg, (Color.Gold with { A = 0.5f }).ToUInt());
+
+                ImGuiUtils.DrawCopyableText(i.ToString());
+
+                ImGui.TableNextColumn(); // Texture
+                if (selected) ImGui.TableSetBgColor(ImGuiTableBgTarget.CellBg, (Color.Gold with { A = 0.5f }).ToUInt());
+
                 ImGui.Image(
-                    new ImTextureID(textureInfo->AtkTexture.KernelTexture->D3D11ShaderResourceView),
-                    new Vector2(
-                        textureInfo->AtkTexture.KernelTexture->ActualWidth,
-                        textureInfo->AtkTexture.KernelTexture->ActualHeight));
+                    new ImTextureID(tex->D3D11ShaderResourceView),
+                    new Vector2(part.Width, part.Height) * version,
+                    new Vector2(part.U, part.V) * version / size,
+                    new Vector2(part.U + part.Width, part.V + part.Height) * version / size);
+
+                if (ImGui.IsItemClicked(ImGuiMouseButton.Left))
+                {
+                    partId = i;
+                    changed |= true;
+                }
+
+                ImGui.TableNextColumn(); // Info
+                if (selected) ImGui.TableSetBgColor(ImGuiTableBgTarget.CellBg, (Color.Gold with { A = 0.5f }).ToUInt());
+
+                ImGui.Text("Position:");
+                ImGui.SameLine();
+                var x = ImGui.GetCursorPosX();
+                ImGuiUtils.DrawCopyableText($"({part.U}, {part.V})", new CopyableTextOptions()
+                {
+                    CopyText = ImGui.IsKeyDown(ImGuiKey.LeftShift)
+                        ? $"new Vector2({part.U}, {part.V})"
+                        : null
+                });
+
+                ImGui.Text("Size:    ");
+                ImGui.SameLine();
+                ImGui.SetCursorPosX(x);
+                ImGuiUtils.DrawCopyableText($"{part.Width}x{part.Height}", new CopyableTextOptions()
+                {
+                    CopyText = ImGui.IsKeyDown(ImGuiKey.LeftShift)
+                        ? $"new Vector2({part.Width}, {part.Height})"
+                        : null
+                });
             }
         }
 
