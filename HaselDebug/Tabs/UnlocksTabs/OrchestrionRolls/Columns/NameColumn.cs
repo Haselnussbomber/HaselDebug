a4014808b640e91cde24cf815d1542155aa85b56
Haselnussbomber <mail@haselnussbomber.de>
2026-01-12 17:05:49 +0100

    Update Orchestrion table

--- HaselDebug/Tabs/UnlocksTabs/OrchestrionRolls/Columns/NameColumn.cs
+++ HaselDebug/Tabs/UnlocksTabs/OrchestrionRolls/Columns/NameColumn.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using HaselCommon.Gui.ImGuiTable;
 using HaselDebug.Utils;
 
@@ -7,6 +9,7 @@ namespace HaselDebug.Tabs.UnlocksTabs.OrchestrionRolls.Columns;
 public partial class NameColumn : ColumnString<OrchestrionRollEntry>
 {
     private readonly UnlocksTabUtils _unlocksTabUtils;
+    private readonly TextService _textService;
 
     [AutoPostConstruct]
     public void Initialize()
@@ -20,19 +23,44 @@ public partial class NameColumn : ColumnString<OrchestrionRollEntry>
     public override unsafe void DrawColumn(OrchestrionRollEntry entry)
     {
         var name = ToName(entry);
-        using (Color.Transparent.Push(ImGuiCol.HeaderActive))
-        using (Color.Transparent.Push(ImGuiCol.HeaderHovered))
-            ImGui.Selectable(name);
+        var selected = OrchestrionManager.Instance()->Mode == OrchestrionMode.Sampling && OrchestrionSampleState.Instance()->TrackId == entry.RowId;
+        var isUnlocked = PlayerState.Instance()->IsOrchestrionRollUnlocked(entry.RowId);
+        var flags = isUnlocked
+            ? ImGuiSelectableFlags.None
+            : ImGuiSelectableFlags.Disabled;
 
-        if (ImGui.IsItemHovered())
+        if (ImGui.Selectable(name, selected, flags))
         {
+            if (selected)
+            {
+                OrchestrionManager.StopSample();
+            }
+            else
+            {
+                OrchestrionManager.PlaySample((ushort)entry.RowId);
+            }
+        }
+
+        if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled))
+        {
+            using var rssb = new RentedSeStringBuilder();
+
+            if (!entry.Row.Description.IsEmpty)
+            {
+                rssb.Builder.AppendLine(entry.Row.Description);
+                rssb.Builder.AppendNewLine();
+            }
+
+            if (isUnlocked)
+                rssb.Builder.Append("Click to preview track");
+            else
+                rssb.Builder.Append(_textService.GetLogMessage(3437));
+
             _unlocksTabUtils.DrawTooltip(
                 entry.UIParamRow.OrchestrionCategory.Value.Icon,
                 name,
                 entry.UIParamRow.OrchestrionCategory.Value.Name,
-                !entry.Row.Description.IsEmpty
-                    ? entry.Row.Description
-                    : default);
+                rssb.Builder.ToReadOnlySeString());
         }
     }
 }
