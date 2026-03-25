1f747d756c7489aacdffd83d1a7204c493377354
Haselnussbomber <mail@haselnussbomber.de>
2026-03-25 22:00:57 +0100

    Oops, that was old

--- HaselDebug/Tabs/PartyFinderListingLogTab.cs
+++ HaselDebug/Tabs/PartyFinderListingLogTab.cs
@@ -1,6 +1,4 @@
-using Dalamud.Game.Gui.PartyFinder.Types;
 using FFXIVClientStructs.FFXIV.Client.Game.Network;
-using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Client.UI.Info;
 using HaselDebug.Abstracts;
 using HaselDebug.Interfaces;
@@ -15,16 +13,14 @@ public unsafe partial class PartyFinderListingLogTab : DebugTab, IDisposable
     private readonly DebugRenderer _debugRenderer;
     private readonly IGameInteropProvider _gameInteropProvider;
 
-    private readonly List<(DateTime Time, Pointer<PartyFinderListingPacket> Packet)> _records = [];
+    private readonly List<(DateTime Time, Pointer<CrossRealmListingSegmentPacket> Packet)> _records = [];
     private Hook<InfoProxyCrossRealm.Delegates.ReceiveListing>? _receiveListingHook;
-    private Hook<AgentLookingForGroup.Delegates.PopulateListingData>? _populateListingHook;
 
     private bool _enabled = false;
 
     public void Dispose()
     {
         _receiveListingHook?.Dispose();
-        _populateListingHook?.Dispose();
         Clear();
     }
 
@@ -38,34 +34,25 @@ public unsafe partial class PartyFinderListingLogTab : DebugTab, IDisposable
 
     private void ReceiveListingDetour(InfoProxyCrossRealm* thisPtr, nint packet)
     {
-        var ptr = (PartyFinderListingPacket*)Marshal.AllocHGlobal(sizeof(PartyFinderListingPacket));
-        *ptr = *(PartyFinderListingPacket*)packet;
-        _records.Add((DateTime.Now, (Pointer<PartyFinderListingPacket>)ptr));
+        var ptr = (CrossRealmListingSegmentPacket*)Marshal.AllocHGlobal(sizeof(CrossRealmListingSegmentPacket));
+        *ptr = *(CrossRealmListingSegmentPacket*)packet;
+        _records.Add((DateTime.Now, (Pointer<CrossRealmListingSegmentPacket>)ptr));
         _receiveListingHook!.Original(thisPtr, packet);
-    }
-
-    private void PopulateListingDataDetour(AgentLookingForGroup* thisPtr, AgentLookingForGroup.Detailed* listingData)
-    {
-        _logger.LogDebug("Listing: {Id} | {_memberContentIds} | {_slotFlags}", listingData->ListingId, string.Join(',', listingData->MemberContentIds.ToArray().Select(x => x.ToString("X"))), string.Join(',', listingData->MemberContentIds.ToArray().Select(x => x.ToString("X"))));
-        _populateListingHook!.Original(thisPtr, listingData);
     }
 
     public override void Draw()
     {
         _receiveListingHook ??= _gameInteropProvider.HookFromAddress<InfoProxyCrossRealm.Delegates.ReceiveListing>((nint)InfoProxyCrossRealm.MemberFunctionPointers.ReceiveListing, ReceiveListingDetour);
-        _populateListingHook ??= _gameInteropProvider.HookFromAddress<AgentLookingForGroup.Delegates.PopulateListingData>((nint)AgentLookingForGroup.MemberFunctionPointers.PopulateListingData, PopulateListingDataDetour);
 
         if (ImGui.Checkbox("Enabled", ref _enabled))
         {
             if (_enabled && !_receiveListingHook.IsEnabled)
             {
                 _receiveListingHook.Enable();
-                _populateListingHook.Enable();
             }
             else if (!_enabled && _receiveListingHook.IsEnabled)
             {
                 _receiveListingHook.Disable();
-                _populateListingHook.Disable();
             }
         }
 
