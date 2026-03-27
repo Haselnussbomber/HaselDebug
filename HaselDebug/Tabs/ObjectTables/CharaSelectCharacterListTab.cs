009fbb5753c96f366afbaa4c48d57de5456bd144
Haselnussbomber <mail@haselnussbomber.de>
2026-03-27 12:28:36 +0100

    Rework CharaSelectCharacterList into table

--- HaselDebug/Tabs/ObjectTables/CharaSelectCharacterListTab.cs
+++ HaselDebug/Tabs/ObjectTables/CharaSelectCharacterListTab.cs
@@ -11,26 +11,44 @@ public unsafe partial class CharaSelectCharacterListTab : DebugTab, IObjectTable
 {
     private readonly DebugRenderer _debugRenderer;
 
+    public override bool DrawInChild => false;
+
     public override void Draw()
     {
         var list = CharaSelectCharacterList.Instance();
         var clientObjectManager = ClientObjectManager.Instance();
-        if (list == null || clientObjectManager == null)
-            return;
+        var agentLobby = AgentLobby.Instance();
+
+        using var table = ImRaii.Table("CharaSelectCharacterTable"u8, 4, ImGuiTableFlags.Borders | ImGuiTableFlags.ScrollY | ImGuiTableFlags.RowBg | ImGuiTableFlags.Resizable);
+        if (!table) return;
+
+        ImGui.TableSetupColumn("ClientObjectIndex"u8, ImGuiTableColumnFlags.WidthFixed, 30);
+        ImGui.TableSetupColumn("ContentId"u8, ImGuiTableColumnFlags.WidthFixed, 120);
+        ImGui.TableSetupColumn("Name"u8, ImGuiTableColumnFlags.WidthFixed, 180);
+        ImGui.TableSetupColumn("GameObject"u8, ImGuiTableColumnFlags.WidthStretch);
+        ImGui.TableSetupScrollFreeze(0, 1);
+        ImGui.TableHeadersRow();
 
         foreach (var item in list->CharacterMapping)
         {
             if (item.ClientObjectIndex == -1)
                 continue;
 
-            ImGui.Text($"{item.ClientObjectIndex}: {item.ContentId:X}");
+            ImGui.TableNextRow();
+            ImGui.TableNextColumn(); // ClientObjectIndex
+            ImGui.Text(item.ClientObjectIndex.ToString());
 
+            ImGui.TableNextColumn(); // ContentId
+            ImGui.Text(item.ContentId.ToString("X"));
+
+            ImGui.TableNextColumn(); // Name
+            if (agentLobby->LobbyData.CharaSelectEntries.TryGetFirst(p => p.Value->ContentId == item.ContentId, out var entry))
+                ImGui.Text(entry.Value->NameString);
+
+            ImGui.TableNextColumn(); // GameObject
             var obj = clientObjectManager->GetObjectByIndex((ushort)item.ClientObjectIndex);
-            if (obj == null)
-                continue;
-
-            ImGui.SameLine();
-            _debugRenderer.DrawPointerType(obj);
+            if (obj != null)
+                _debugRenderer.DrawPointerType(obj);
         }
     }
 }
