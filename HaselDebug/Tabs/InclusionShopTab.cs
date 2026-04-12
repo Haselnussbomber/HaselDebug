0202d4061cde950e2ad888169ecf6b8e5f61bdeb
Haselnussbomber <mail@haselnussbomber.de>
2026-04-12 14:45:33 +0200

    Update ClientStructs

--- HaselDebug/Tabs/InclusionShopTab.cs
+++ HaselDebug/Tabs/InclusionShopTab.cs
@@ -1,4 +1,4 @@
-using FFXIVClientStructs.FFXIV.Component.GUI;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using HaselDebug.Abstracts;
 using HaselDebug.Interfaces;
 using HaselDebug.Services;
@@ -13,41 +13,12 @@ public unsafe partial class InclusionShopTab : DebugTab
 
     public override void Draw()
     {
-        if (!TryGetAddon<AtkUnitBase>("InclusionShop", out var addon))
+        if (!TryGetAddon<AddonInclusionShop>("InclusionShop", out var addon))
         {
-            ImGui.Text("No shop open!"u8);
+            ImGui.Text("No InclusionShop open!"u8);
             return;
         }
 
-        _debugRenderer.DrawPointerType(addon->AtkValues, typeof(InclusionShopAtkValues), new NodeOptions());
+        _debugRenderer.DrawPointerType(addon->AtkValues, typeof(AddonInclusionShop.InclusionShopAtkValues), new NodeOptions());
     }
-}
-
-[GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x10 * 2939)]
-public partial struct InclusionShopAtkValues
-{
-    [FieldOffset(0x10 * 296)] public AtkValue PinnedCurrencyIconId;
-    [FieldOffset(0x10 * 297)] public AtkValue PinnedCurrencyCount;
-    [FieldOffset(0x10 * 298)] public AtkValue ItemCount;
-    [FieldOffset(0x10 * 299), FixedSizeArray] internal FixedSizeArray60<InclusionShopItem> _items;
-}
-
-[GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x10 * 18)]
-public partial struct InclusionShopItem
-{
-    [FieldOffset(0x10 * 0)] public AtkValue AtkValue0;
-    [FieldOffset(0x10 * 1)] public AtkValue ItemId;
-    [FieldOffset(0x10 * 2)] public AtkValue IconId;
-    [FieldOffset(0x10 * 3)] public AtkValue Stacksize;
-    [FieldOffset(0x10 * 4)] public AtkValue AmountOwned;
-    [FieldOffset(0x10 * 5)] public AtkValue GiveCount;
-    [FieldOffset(0x10 * 6), FixedSizeArray] internal FixedSizeArray3<AtkValue> _giveItemId;
-    [FieldOffset(0x10 * 9), FixedSizeArray] internal FixedSizeArray3<AtkValue> _giveIconId;
-    [FieldOffset(0x10 * 12), FixedSizeArray] internal FixedSizeArray3<AtkValue> _giveAmount;
-    [FieldOffset(0x10 * 14)] public AtkValue AtkValue14;
-    [FieldOffset(0x10 * 15)] public AtkValue MaxAmount;
-    [FieldOffset(0x10 * 16)] public AtkValue Flags; // 0b10 = CanSelectAmount
-    [FieldOffset(0x10 * 17)] public AtkValue Index;
 }
