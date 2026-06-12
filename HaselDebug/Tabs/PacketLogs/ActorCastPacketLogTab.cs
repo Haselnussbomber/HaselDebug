b4723fa1577ff199cca342061ca9f8ad2d9db879
Haselnussbomber <mail@haselnussbomber.de>
2026-06-12 13:37:02 +0200

    Add ActorCastPacketLogTab

---
+++ HaselDebug/Tabs/PacketLogs/ActorCastPacketLogTab.cs
@@ -0,0 +1,67 @@
+using FFXIVClientStructs.FFXIV.Client.Game.Network;
+using FFXIVClientStructs.FFXIV.Client.Network;
+using HaselDebug.Abstracts;
+using HaselDebug.Interfaces;
+
+namespace HaselDebug.Tabs.PacketLogs;
+
+[RegisterSingleton<IPacketLogTab>(Duplicate = DuplicateStrategy.Append), AutoConstruct]
+public unsafe partial class ActorCastPacketLogTab : PacketLogTab<ActorCastPacket>, IDisposable
+{
+    private Hook<PacketDispatcher.Delegates.HandleActorCastPacket>? _hook;
+
+    public void Dispose()
+    {
+        _hook?.Dispose();
+        Clear();
+    }
+
+    private void HandleActorCastPacketDetour(uint entityId, ActorCastPacket* packet)
+    {
+        AddRecord(*packet);
+        _hook!.Original(entityId, packet);
+    }
+
+    public override void Draw()
+    {
+        _hook ??= _gameInteropProvider.HookFromAddress<PacketDispatcher.Delegates.HandleActorCastPacket>((nint)PacketDispatcher.MemberFunctionPointers.HandleActorCastPacket, HandleActorCastPacketDetour);
+
+        var enabled = IsPacketLogEnabled;
+        if (ImGui.Checkbox("Enabled", ref enabled))
+            TogglePacketLog();
+
+        ImGui.SameLine();
+        if (ImGui.Button("Clear"))
+            Clear();
+
+        using var table = ImRaii.Table("ActorCastPacketLogTable"u8, 2, ImGuiTableFlags.Borders | ImGuiTableFlags.ScrollY | ImGuiTableFlags.RowBg | ImGuiTableFlags.Resizable);
+        if (!table) return;
+
+        ImGui.TableSetupColumn("Time"u8, ImGuiTableColumnFlags.WidthFixed, 100);
+        ImGui.TableSetupColumn("Packet"u8, ImGuiTableColumnFlags.WidthStretch);
+        ImGui.TableSetupScrollFreeze(0, 1);
+        ImGui.TableHeadersRow();
+
+        foreach (var (index, time, packet) in Records)
+        {
+            ImGui.TableNextRow();
+            ImGui.TableNextColumn(); // Time
+            ImGui.Text(time.ToLongTimeString());
+
+            ImGui.TableNextColumn(); // Packet
+            _debugRenderer.DrawPointerType(packet);
+        }
+    }
+
+    public override void EnablePacketLog()
+    {
+        _hook!.Enable();
+        IsPacketLogEnabled = _hook.IsEnabled;
+    }
+
+    public override void DisablePacketLog()
+    {
+        _hook!.Disable();
+        IsPacketLogEnabled = _hook.IsEnabled;
+    }
+}
