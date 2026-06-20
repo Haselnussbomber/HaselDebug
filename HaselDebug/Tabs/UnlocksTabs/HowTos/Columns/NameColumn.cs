e8f1f522619ee34dd2f56a800a8582e1c57c0aec
Haselnussbomber <mail@haselnussbomber.de>
2026-06-20 17:27:07 +0200

    Update ClientStructs

--- HaselDebug/Tabs/UnlocksTabs/HowTos/Columns/NameColumn.cs
+++ HaselDebug/Tabs/UnlocksTabs/HowTos/Columns/NameColumn.cs
@@ -25,10 +25,7 @@ public partial class NameColumn : ColumnString<HowTo>
 
         if (ImGui.Selectable(ToName(row), false, isUnlocked ? ImGuiSelectableFlags.None : ImGuiSelectableFlags.Disabled))
         {
-            var agentHowTo = AgentHowToExt.Instance();
-            agentHowTo->RowId = row.RowId;
-            agentHowTo->Unk44 = 0;
-            agentHowTo->AgentInterface.Show();
+            AgentHowTo.Instance()->OpenHowTo(row.RowId);
         }
 
         if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled))
@@ -38,27 +35,4 @@ public partial class NameColumn : ColumnString<HowTo>
             _unlocksTabUtils.DrawHowToTooltip(row);
         }
     }
-}
-
-// [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x98)]
-public unsafe partial struct AgentHowToExt
-{
-    public static AgentHowToExt* Instance() => (AgentHowToExt*)AgentModule.Instance()->GetAgentByInternalId(AgentId.Howto);
-
-    [FieldOffset(0x00)] public AgentInterface AgentInterface;
-    [FieldOffset(0x40)] public uint RowId;
-    [FieldOffset(0x44)] public uint Unk44;
-
-    [FieldOffset(0x49)] public bool IsPadMode;
-
-    [FieldOffset(0x7C)] public uint PageCount;
-
-    [FieldOffset(0x88)] public nint HowToRowPtr;
-
-    // [MemberFunction("E8 ?? ?? ?? ?? BA ?? ?? ?? ?? 48 8D 4D ?? 8B D8 E8 ?? ?? ?? ?? 89 5D ?? 48 8D 4D ?? 8B 9F")]
-    // public partial uint GetAdjustedHowToIconId(nint rowPtr, byte startTown, byte grandCompany);
-
-    // [MemberFunction("E8 ?? ?? ?? ?? EB ?? BA ?? ?? ?? ?? C7 87")]
-    // public static partial CStringPointer GetAdjustedHowToText(UIModule* thisPtr, nint rowPtr, byte startTown, byte grandCompany);
 }
