cb5b802a649f98838a053b87f0ed2bda2b3e8ca8
Haselnussbomber <mail@haselnussbomber.de>
2026-07-26 18:05:45 +0200

    Add Beast Tribes Todo args tab

---
+++ HaselDebug/Tabs/TodoArgsTab.cs
@@ -0,0 +1,166 @@
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using FFXIVClientStructs.FFXIV.Client.Game.Event;
+using HaselDebug.Abstracts;
+using HaselDebug.Interfaces;
+
+namespace HaselDebug.Tabs;
+
+[RegisterSingleton<IDebugTab>(Duplicate = DuplicateStrategy.Append), AutoConstruct]
+public unsafe partial class TodoArgsTab : DebugTab
+{
+    private readonly ExcelService _excelService;
+    private readonly TextService _textService;
+
+    public override void Draw()
+    {
+        var questManager = QuestManager.Instance();
+        var localPlayer = Control.GetLocalPlayer();
+        if (localPlayer == null) return;
+
+        foreach (ref var questWork in questManager->NormalQuests)
+        {
+            if (questWork.QuestId == 0)
+                continue;
+
+            if (!_excelService.TryGetRow<Quest>((uint)questWork.QuestId | 0x10000, out var quest))
+                continue;
+
+            if (!(questManager->GetDailyQuestById(questWork.QuestId) != null || quest.BeastTribe.RowId != 0)) // check if daily or tribal quest
+                continue;
+
+            var sequence = questWork.Sequence;
+            if (!quest.TodoParams.TryGetFirst(param => param.ToDoCompleteSeq == sequence, out var todoParams, out var todoOffset))
+                continue;
+
+            var questEventHandler = (QuestEventHandler*)EventFramework.Instance()->GetEventHandlerById(quest.RowId);
+            if (questEventHandler == null)
+                continue;
+
+            ImGui.Text($"#{questWork.QuestId} ({_textService.GetQuestName((uint)questWork.QuestId | 0x10000)}) (Sequence {questWork.Sequence})");
+
+            using var indent = ImRaii.PushIndent();
+
+            uint GetScriptArg(string scriptArgName = "RITEM1")
+            {
+                return quest.QuestParams.TryGetFirst(p => p.ScriptInstruction == scriptArgName, out var param) ? param.ScriptArg : 0;
+            }
+
+            var todoCount = todoParams.ToDoQty;
+            for (var todoIndex = (byte)todoOffset; todoIndex < todoOffset + todoCount; todoIndex++)
+            {
+                uint numHave, numNeeded, resultItemId;
+                questEventHandler->GetTodoArgs(localPlayer, todoIndex, &numHave, &numNeeded, &resultItemId);
+
+                // for older quests that don't return the item id in GetTodoArgs
+                switch ((questWork.QuestId, todoIndex))
+                {
+                    // Ixal
+                    case (1487, 2):
+                    case (1487, 3):
+                        resultItemId = GetScriptArg("RITEM2");
+                        break;
+                    case (1493, 12):
+                        resultItemId = GetScriptArg("RITEM3");
+                        break;
+                    case (1488, 3):
+                    case (1489, 4):
+                    case (1491, 2):
+                    case (1493, 1):
+                    case (1494, 3):
+                    case (1495, 3):
+                    case (1496, 2):
+                    case (1497, 2):
+                    case (1498, 4):
+                    case (1499, 4):
+                    case (1500, 2):
+                    case (1501, 2):
+                    case (1502, 3):
+                    case (1503, 2):
+                    case (1504, 2):
+                    case (1505, 2):
+                    case (1506, 2):
+                    case (1507, 2):
+                    case (1508, 2):
+                    case (1509, 3):
+                    case (1510, 4):
+                    case (1511, 4):
+                    case (1512, 3):
+                    case (1513, 3):
+                    case (1514, 2):
+                    case (1515, 3):
+                    case (1516, 3):
+                    case (1517, 2):
+                    case (1518, 3):
+                    case (1519, 3):
+                    case (1520, 3):
+                    case (1521, 4):
+                    case (1522, 4):
+                    case (1523, 3):
+                    case (1566, 2):
+                    case (1567, 2):
+                    case (1568, 2):
+                        resultItemId = GetScriptArg();
+                        break;
+
+                    // Moogle
+                    case (2290, 1):
+                    case (2291, 1):
+                    case (2292, 3):
+                    case (2293, 3):
+                    case (2294, 3):
+                    case (2296, 1):
+                    case (2298, 1):
+                    case (2299, 1):
+                    case (2300, 1):
+                    case (2301, 1):
+                    case (2303, 1):
+                    case (2304, 2):
+                    case (2305, 4):
+                    case (2307, 1):
+                    case (2310, 2):
+                    case (2311, 3):
+                    case (2313, 3):
+                    case (2314, 2):
+                    case (2316, 2):
+                    case (2317, 1):
+                    case (2318, 5):
+                    case (2320, 3):
+                    case (2322, 5):
+                    case (2324, 4):
+                    case (2325, 5):
+                    case (2326, 7):
+                        resultItemId = GetScriptArg();
+                        break;
+
+                    // Namazu
+                    case (3098, 1):
+                    case (3099, 1):
+                    case (3100, 1):
+                    case (3101, 1):
+                    case (3106, 1):
+                    case (3107, 1):
+                    case (3108, 1):
+                    case (3109, 1):
+                    case (3110, 1):
+                    case (3111, 1):
+                    case (3112, 1):
+                    case (3113, 1):
+                    case (3114, 1):
+                    case (3117, 1):
+                    case (3120, 1):
+                    case (3123, 1):
+                    case (3126, 1):
+                    case (3129, 1):
+                    case (3130, 1):
+                        resultItemId = GetScriptArg("QST_PRODUCT_ITEM");
+                        break;
+                }
+
+                resultItemId %= 1000000;
+
+                ImGui.Text($"TodoArgs #{todoIndex}: {numHave}/{numNeeded} of {resultItemId}");
+            }
+        }
+    }
+}
